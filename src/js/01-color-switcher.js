af87ca2914645337f8dd7a8c77d95fa08cd994f4
Rowell Calipay <30309887+crux16@users.noreply.github.com>
2023-11-15 00:54:43 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,27 +9,84 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 //     RANDOM BACKGROUND COLOR CHANGE RUNTIME
 // ==============================================================
 
-const stopBtn = document.querySelector('[data-stop]');
-const startBtn = document.querySelector('[data-start]');
-const loadBg = document.body;
-stopBtn.disabled = true; 
-let loadHex  = null;
+const stopBtn = document.querySelector('button[data-stop]');
+const startBtn = document.querySelector('button[data-start]');
+const buttons = document.querySelectorAll('button');
 
+// default Button and default Interval variable
+stopBtn.disabled = true; 
+let loadHex;
+
+// optional default style
+startBtn.style.backgroundColor = "rgba(0,255,0,1)";
+stopBtn.style.backgroundColor = "rgba(255,0,0,0.5)";
+
+// general style
+buttons.forEach((button)=>{
+        button.style.color = "#fcf";
+        button.style.fontSize = "20px";
+        button.style.border = "1px solid #fff";
+        button.style.padding = "5px 15px";
+        button.style.boxShadow = "0 0 1px #000";
+        button.style.textShadow = "0 0 3px #000";
+        button.style.transition = "text-decoration-color ease 1sec";
+})
+
+// ===============================================
+//  METHOD or FUNCTION
+// ===============================================
+
+// function set a background color with transition
 const hexPlay = () => {
-        loadBg.style.backgroundColor = getRandomHexColor();
-        loadBg.style.transition = "background-color ease-in-out 500ms"; 
+        document.body.style.backgroundColor = getRandomHexColor();
+        document.body.style.transition = "background-color ease-in-out 500ms"; 
 };  
 
-startBtn.addEventListener('click', function() {
+// addtional function to line-through text
+const disabledBtn = (d) => {
+        buttons.forEach((button)=>{button.style.textDecoration='none';button.style.textDecorationColor='transparent';});
+        d.style.textDecoration='line-through #eee';
+};
+
+disabledBtn(stopBtn);
+
+// ================================================
+
+// Event action click for button Start
+startBtn.addEventListener('click', function(event) {
+        // set button into disabled and enabled
         startBtn.disabled = true;
         stopBtn.disabled = false;
+
+        // line text style
+        disabledBtn(event.currentTarget);
+
+        // addtional styling
+        startBtn.style.backgroundColor = "rgba(0,255,0,0.3)";
+        stopBtn.style.backgroundColor = "rgba(255,0,0,1)";
+
+        // reference variable for setInterval() method
         loadHex = setInterval(hexPlay,1000);
+
+        // Notify Play mode message
         Notify.success('PLAY');
 });
 
-stopBtn.addEventListener('click', function() {
+stopBtn.addEventListener('click', function(event) {
+        // set button into disabled and enabled
         stopBtn.disabled = true; 
         startBtn.disabled = false;
-        Notify.failure('STOP');
+        
+        // line text style
+        disabledBtn(event.currentTarget);
+
+        // addtional styling
+        startBtn.style.backgroundColor = "rgba(0,255,0,1)";
+        stopBtn.style.backgroundColor = "rgba(255,0,0,0.5)";
+
+        // by setting the variable inside the clearInterval() method will initiate clearing the variable 
         clearInterval(loadHex);
+        
+        // Notify Stop mode message
+        Notify.failure('STOP');
 });
