ce619571de6f170b00c0abb833d262b35e178a47
Rowell Calipay <30309887+crux16@users.noreply.github.com>
2023-11-14 23:41:48 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,30 +1,35 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+// Notify
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 // ==============================================================
-//     RANDOM BACKGROUND COLOR RUNTIME
+//     RANDOM BACKGROUND COLOR CHANGE RUNTIME
 // ==============================================================
 
 const stopBtn = document.querySelector('[data-stop]');
 const startBtn = document.querySelector('[data-start]');
 const loadBg = document.body;
 stopBtn.disabled = true; 
-let hexAction = false;
+let loadHex  = null;
 
 const hexPlay = () => {
         loadBg.style.backgroundColor = getRandomHexColor();
         loadBg.style.transition = "background-color ease-in-out 500ms"; 
-};
+};  
 
 startBtn.addEventListener('click', function() {
         startBtn.disabled = true;
         stopBtn.disabled = false;
         loadHex = setInterval(hexPlay,1000);
+        Notify.success('PLAY');
 });
 
 stopBtn.addEventListener('click', function() {
         stopBtn.disabled = true; 
         startBtn.disabled = false;
+        Notify.failure('STOP');
         clearInterval(loadHex);
-        console.log("set:", hexAction);
 });
